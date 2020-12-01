04c668968310165750edceb638f7dfdc06904f07
farhadshirmardi <shirmardi7@gmail.com>
2020-12-01 09:23:11 +0330

    -

---
+++ app/Constants/AnnouncementType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Constants;
+
+use ReflectionClass;
+use ReflectionException;
+
+class AnnouncementType
+{
+    const PUBLIC = 1;
+    const PRIVATE = 2;
+
+    /**
+     * @return array
+     * @throws ReflectionException
+     */
+    public static function toArray()
+    {
+        $oClass = new ReflectionClass(__CLASS__);
+        return $oClass->getConstants();
+    }
+
+    public static function getEnum($enum)
+    {
+        switch ($enum) {
+            case self::PUBLIC:
+                return 'عمومی';
+            case self::PRIVATE:
+                return 'خصوصی';
+            default:
+                return '';
+        }
+    }
+}
