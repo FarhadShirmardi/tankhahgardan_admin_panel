1c3e95ebc5d5f427d76f68381d9f2f8999482388
farhad <shirmardi7@gmail.com>
2023-01-05 11:38:37 +0330

    -

--- app/Models/PanelUser.php
+++ app/Models/PanelUser.php
@@ -2,8 +2,13 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class PanelUser extends Authenticatable
+class PanelUser extends Authenticatable implements FilamentUser
 {
+    public function canAccessFilament(): bool
+    {
+        return true;
+    }
 }
